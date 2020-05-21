dfdb4d2426b52ae961faad17086bd276682999c2
*Niler <nilerbarcelos@gmail.com>
2020-05-21 17:00:22 -0300

    Creating author model

--- library/author/models.py
+++ library/author/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def author_id(self):
+        return self.id
