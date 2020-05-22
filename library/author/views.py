91b6a76de02b9109cc355f99d8aaf456fed9d79d
*Niler <nilerbarcelos@gmail.com>
2020-05-22 12:03:14 -0300

    Create get authors view

--- library/author/views.py
+++ library/author/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from library.author.factories.author_gateway_django_factory import AuthorGatewayDjangoFactory
+from library.author.presenters.list_authors_presenter_django import ListAuthorsPresenterDjango
+
+
+class AuthorView(View):
+    def get(self, request):
+        author_gateway = AuthorGatewayDjangoFactory.make()
+        page = int(request.GET.get('page', 1))
+        name = request.GET.get('name', None)
+        authors, total_table_records = author_gateway.get_authors(name=name, page=page)
+
+        presenter = ListAuthorsPresenterDjango()
+        presenter.present_authors(authors, total_table_records)
+
+        return presenter.create_response()
